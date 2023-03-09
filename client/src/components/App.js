facec04ed7798f3866aace4caa70725907979033
Brandon Cho <hjun1114@gmail.com>
2023-03-09 00:00:02 -0500

    adding router v5

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,13 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
 const App = () => {
-	return <div>Hello from App</div>;
+	return (
+		<div>
+			<BrowserRouter>
+				<Route path="/" exact component={App} />
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
