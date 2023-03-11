6ccd248c9101b6a7244fdc11a812f417c3131dd7
Brandon Cho <hjun1114@gmail.com>
2023-03-10 23:55:18 -0500

    adding Navigation header template with router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import Header from "./Header";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
@@ -7,8 +8,9 @@ import StreamShow from "./streams/StreamShow";
 
 const App = () => {
 	return (
-		<div>
+		<div className="ui container">
 			<BrowserRouter>
+				<Header />
 				<Route path="/" exact component={StreamList} />
 				<Route path="/streams/show" component={StreamShow} />
 				<Route path="/stream/edit" exact component={StreamEdit} />
